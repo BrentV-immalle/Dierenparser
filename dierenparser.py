c5c70543287ccbec8038476e0b0df765335bc1a0
Verlinden Brent <brent.verlinden@immalle.eu>
2019-11-04 11:59:58 +0100

    Dier omgezet naar een dataclass. (Refactory).

--- dierenparser.py
+++ dierenparser.py
@@ -1,15 +1,17 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class Dier:
-
-    def __init__(self):
-        self.naam = ""
-        self.soort = ""
-        self.aantalPoten = 0
-        self.kleur = ""
-        self.geluid = ""
-
-    def print(self):
-        print(
-            f'Het dier heet [{self.naam}], is van soort [{self.soort}], heeft [{self.aantalpoten}] poten, is [{self.kleur}] en maakt dit geluid: [{self.geluid}]!')
+    # def __init__(self):
+    naam: ""
+    soort: ""
+    aantalPoten: 0
+    kleur: ""
+    geluid: ""
+    # def print(self):
+    # print(
+    # f'Het dier heet [{self.naam}], is van soort [{self.soort}], heeft [{self.aantalpoten}] poten, is [{self.kleur}] en maakt dit geluid: [{self.geluid}]!')
 
 
 def parse_line(line):
