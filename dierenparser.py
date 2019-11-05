7143cfceb087f433a313a4c243a44af0b20e81ad
Verlinden Brent <brent.verlinden@immalle.eu>
2019-11-05 12:00:48 +0100

    kleine verandering

--- dierenparser.py
+++ dierenparser.py
@@ -14,13 +14,13 @@ class Dier:
     # f'Het dier heet [{self.naam}], is van soort [{self.soort}], heeft [{self.aantalpoten}] poten, is [{self.kleur}] en maakt dit geluid: [{self.geluid}]!')
 
 
-def parse_line(line):
+def parse_line(line: str) -> Dier:
     naam, soort, aantalpoten, kleur, geluid = line.split(' - ')
     d = Dier(naam, soort, int(aantalpoten), kleur, geluid)
     return d
 
 
-def parse_text(str):
+def parse_text(str: str) -> [Dier]:
     dieren = []
     for line in str.splitlines():
         d = parse_line(line)
@@ -28,6 +28,7 @@ def parse_text(str):
     return dieren
 
 
+# rb is read binary kan ook, r en w vooral. read, write. Normaal f.close() fs maar moet niet want je doet with open. Automatisch gesloten als we klaar zijn ermee te werken met with.
 if __name__ == '__main__':
     dieren = []
     with open('dieren.txt', 'r') as f:
